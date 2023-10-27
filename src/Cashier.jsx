f3b847a20cbe9a86d91839905a66036e2e8e694a
A L <allenli512@gmail.com>
2023-10-26 22:17:07 -0500

    table needs editing, wrapping up

--- src/Cashier.jsx
+++ src/Cashier.jsx
@@ -5,6 +5,13 @@ import './Cashier.css'
 import CustomerInput from './CustomerInput';
 import axios from 'axios';
 import { prodArray } from './productArray';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
 
 function populateButtons(handleClick) {
@@ -18,22 +25,22 @@ function populateButtons(handleClick) {
         const buttonPrice = product.price;
         const buttonID = product.id;
         const buttonMods = product.ingredients;
-      
+
         order_buttons.push(
-          <button
-            key={i}
-            className='grid-button'
-            onClick={() => handleClick(buttonLabel, buttonPrice, buttonMods, buttonID)}
-          >
-            {buttonLabel}
-          </button>
+            <button
+                key={i}
+                className='grid-button'
+                onClick={() => handleClick(buttonLabel, buttonPrice, buttonMods, buttonID)}
+            >
+                {buttonLabel}
+            </button>
         );
-      }
+    }
 
     return order_buttons;
 }
 function Header() {
-    
+
 
     // here I have all my states and handlers
     const [currentView, setCurrentView] = useState("");
@@ -50,7 +57,6 @@ function Header() {
         setPrice(btn_price);
 
     }
-
     const handleViewChange = (view) => {
         setCurrentView(view);
     }
@@ -71,8 +77,8 @@ function Header() {
                     <li>Log Out</li>
                 </ul>
             </nav>
-                {currentView === 'placeOrders' && <PlaceOrders/>}; 
-                {currentView === 'viewOrders' && <ViewOrders/>}; 
+            {currentView === 'placeOrders' && <PlaceOrders />};
+            {currentView === 'viewOrders' && <ViewOrders />};
             <div className='placeorders_page'>
                 <div className='place_left_side'>
                     <h1 className='menu-title'>Menu Items</h1>
@@ -82,19 +88,52 @@ function Header() {
                 </div>
                 <div className='place_right_side'>
                     <div>
-                        <CustomerInput onInputChange = {handleInputChange}/>
+                        <CustomerInput onInputChange={handleInputChange} />
                         <p>Quantity: {quantity} </p>
                     </div>
                     <div className='selectedAttributes'>
                         <h1 style={{ display: 'inline' }}>Selected Item: </h1>
-                        <h1 style={{ display: 'inline' }}>{selectedButton}</h1>    
-                        <h1>Price: {price}</h1>                    
+                        <h1 style={{ display: 'inline' }}>{selectedButton}</h1>
+                        <h1>Price: {price}</h1>
                     </div>
-
+                    <DenseTable/>
                 </div>
             </div>
         </div>
 
+    );
+}
+function DenseTable() {
+    return (
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Dessert (100g serving)</TableCell>
+                        <TableCell align="right">Calories</TableCell>
+                        <TableCell align="right">Fat&nbsp;(g)</TableCell>
+                        <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+                        <TableCell align="right">Protein&nbsp;(g)</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {prodArray.map((row) => (
+                        <TableRow
+                            key={row.name}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                            <TableCell component="th" scope="row">
+                                {row.name}
+                            </TableCell>
+                            <TableCell align="right">{row.calories}</TableCell>
+                            <TableCell align="right">{row.fat}</TableCell>
+                            <TableCell align="right">{row.carbs}</TableCell>
+                            <TableCell align="right">{row.protein}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
     );
 }
 
