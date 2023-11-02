89f13009d029d2d1c2df5f4ea1b9a91c7c2d606b
A L <allenli512@gmail.com>
2023-11-02 18:47:30 -0500

    homescreen almost done

--- src/Home.jsx
+++ src/Home.jsx
@@ -20,7 +20,62 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Form from 'react-bootstrap/Form';
 
+function LogIn() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleUsernameChange = (e) => {
+        setUsername(e.target.value);
+    };
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value);
+    };
+
+    const handleLogIn = () => {
+        // Implement your login logic here
+        console.log('Logging in with Username:', username, 'Password:', password);
+    };
+
+    const handleClear = () => {
+        setUsername('');
+        setPassword('');
+    };
+
+    return (
+        <div className='login_container'>
+            <div className='username'>
+                <label>Username:</label>
+                <input
+                    type="text"
+                    value={username}
+                    onChange={handleUsernameChange}
+                />
+            </div>
+            <div className='password'>
+                <label>Password:</label>
+                <input
+                    type="password"
+                    value={password}
+                    onChange={handlePasswordChange}
+                />
+            </div>
+            <div className='buttons'>
+                <button onClick={handleLogIn}>Log In</button>
+                <button onClick={handleClear}>Clear</button>
+            </div>
+        </div>
+    );
+}
+
+
 function Home() {
+    const [loginVisible, setLoginVisible] = useState(false);
+
+    const toggleLogin = () => {
+        setLoginVisible(!loginVisible);
+    };
+
     return (
         <div>
             <nav className='header'>
@@ -35,19 +90,25 @@ function Home() {
                 </ul>
             </nav>
             <div className='body'>
-                <div class="container">
-                    <div class="big_logo">
+                <div className='container'>
+                    <div className='big_logo'>
                         <h1>WELCOME TO SHARE TEA</h1>
                     </div>
-                    <div class="button-container">
-                        <button>Log In</button>
+                    <div className='button-container'>
+                        <button onClick={toggleLogin}>Log In</button>
                         <button>Start Ordering</button>
                     </div>
+                    {loginVisible && (
+                        <div className='login-area'>
+                            <LogIn />
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
 
     )
 }
-
 export default Home
+export {LogIn}
+
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Cashier, { Header } from './Cashier.jsx'
 import Test from './test.jsx'
-import Home from './Home.jsx'
+import Home, {LogIn} from './Home.jsx'
 // import './index.css'
 import Button from 'react-bootstrap/Button'
 import "bootstrap/dist/css/bootstrap.min.css"
@@ -12,7 +12,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <Header />
     <Cashier /> */}
-    <Home />
+    {/* <Home /> */}
+    <LogIn />
+    
     {/* /* <Test/> */}
   </React.StrictMode>
 )
