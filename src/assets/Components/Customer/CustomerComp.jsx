be8d1ad73e7b92b0853120d0d007c077edc268c6
gabrielmarshall327 <gabrielmarshall327@gmail.com>
2023-11-07 21:47:42 -0600

    trying to get redeploy to work again

--- src/assets/Components/Customer/CustomerComp.jsx
+++ src/assets/Components/Customer/CustomerComp.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-//import Cashier, { Header } from "./Cashier.jsx";
 import Customer from "./Customer.jsx";
 import Weather from "./Weather.jsx";
 
 export const CustomerComp = () => {
-    ReactDOM.createRoot(document.getElementById("root")).render(
-      <React.StrictMode>
-        <Customer />
-        <Weather />
-      </React.StrictMode>
-    );
-}
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+      <Customer />
+      {/*<Weather /> */}
+    </React.StrictMode>
+  );
+};
