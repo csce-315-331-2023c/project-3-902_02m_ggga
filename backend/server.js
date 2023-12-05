20b7a9e4c4cdc93426969ae6c0c6893d8d0d7e8f
gabrielmarshall327 <gabrielmarshall327@gmail.com>
2023-12-05 17:17:25 -0600

    changed server again

--- backend/server.js
+++ backend/server.js
@@ -111,7 +111,6 @@ app.post("/placeorder/", async (req, res) => {
   res.send("hello this is a test");
 });
 
-
 //USED FOR LOGIN
 app.get("/getAccessToken", async function (req, res) {
   console.log(req.query.code);
@@ -161,8 +160,7 @@ app.get("/getUserData", async function (req, res) {
     });
 });
 
-
-app.get('/verifyEmployee', async function (req, res) {
+app.get("/verifyEmployee", async function (req, res) {
   const gitidValue = req.query.gitid;
   const query = "SELECT manager FROM employees WHERE gitid = $1";
   const result = await pool
@@ -219,7 +217,13 @@ app.post("/employees", async (req, res) => {
       VALUES ($1, $2, $3, $4, $5)
       RETURNING *;`;
 
-    const result = await pool.query(newEmployeeQuery, [name, hours_worked, salary, position, manager]);
+    const result = await pool.query(newEmployeeQuery, [
+      name,
+      hours_worked,
+      salary,
+      position,
+      manager,
+    ]);
     res.status(201).json(result.rows[0]);
   } catch (error) {
     console.error("Error adding new employee", error);
@@ -227,41 +231,49 @@ app.post("/employees", async (req, res) => {
   }
 });
 
-
 app.delete("/employees/:id", async (req, res) => {
   try {
     const { id } = req.params;
-    const deleteQuery = 'DELETE FROM employees WHERE employee_id = $1 RETURNING *;';
+    const deleteQuery =
+      "DELETE FROM employees WHERE employee_id = $1 RETURNING *;";
 
     const result = await pool.query(deleteQuery, [id]);
     if (result.rows.length === 0) {
       return res.status(404).json({ message: "Employee not found" });
     }
 
-    res.json({ message: "Employee deleted successfully", employee: result.rows[0] });
+    res.json({
+      message: "Employee deleted successfully",
+      employee: result.rows[0],
+    });
   } catch (error) {
     console.error("Error deleting employee", error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
-
 app.post("/inventory", async (req, res) => {
   try {
-    const { name, price_per_unit, quantity, last_bought_date, minimum } = req.body;
+    const { name, price_per_unit, quantity, last_bought_date, minimum } =
+      req.body;
     const newInventoryQuery = `
       INSERT INTO inventory (name, price_per_unit, quantity, last_bought_date, minimum)
       VALUES ($1, $2, $3, $4, $5)
       RETURNING *;`;
 
-    const result = await pool.query(newInventoryQuery, [name, price_per_unit, quantity, last_bought_date, minimum]);
+    const result = await pool.query(newInventoryQuery, [
+      name,
+      price_per_unit,
+      quantity,
+      last_bought_date,
+      minimum,
+    ]);
     res.status(201).json(result.rows[0]);
   } catch (error) {
     console.error("Error adding new inventory item", error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
-
 
 app.delete("/inventory/:id", async (req, res) => {
   try {
@@ -273,7 +285,10 @@ app.delete("/inventory/:id", async (req, res) => {
       return res.status(404).json({ message: "Item not found" });
     }
 
-    res.json({ message: "Item deleted successfully", deletedItem: result.rows[0] });
+    res.json({
+      message: "Item deleted successfully",
+      deletedItem: result.rows[0],
+    });
   } catch (error) {
     console.error("Error deleting item", error);
     res.status(500).json({ error: "Internal Server Error" });
@@ -292,7 +307,12 @@ app.post("/products", async (req, res) => {
       RETURNING *;`;
 
     // Execute the query with the product information
-    const result = await pool.query(newProductQuery, [name, price, ingredients, image_url]);
+    const result = await pool.query(newProductQuery, [
+      name,
+      price,
+      ingredients,
+      image_url,
+    ]);
 
     // Respond with the newly inserted product row
     res.status(201).json(result.rows[0]);
@@ -312,14 +332,15 @@ app.delete("/products/:id", async (req, res) => {
       return res.status(404).json({ message: "Item not found" });
     }
 
-    res.json({ message: "Product deleted successfully", deletedItem: result.rows[0] });
+    res.json({
+      message: "Product deleted successfully",
+      deletedItem: result.rows[0],
+    });
   } catch (error) {
     console.error("Error deleting product", error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
-
-
 
 app.get("/sales-data", async (req, res) => {
   try {
@@ -339,13 +360,16 @@ app.get("/sales-data", async (req, res) => {
 app.get("/product-sales-data", async (req, res) => {
   const productName = req.query.productName;
   try {
-    const result = await pool.query(`
+    const result = await pool.query(
+      `
       SELECT date_trunc('day', order_date) AS day, COUNT(*) AS number_of_sales
       FROM orders
       WHERE items @> $1::text[] -- Assuming items is a text array and contains the product names
       GROUP BY day
       ORDER BY day
-    `, [`{"${productName}"}`]); // This binds the productName as an array element for the query
+    `,
+      [`{"${productName}"}`]
+    ); // This binds the productName as an array element for the query
     res.json(result.rows);
   } catch (error) {
     console.error("Error fetching sales data for product", error);
@@ -356,7 +380,7 @@ app.get("/product-sales-data", async (req, res) => {
 app.get("/weather", async (req, res) => {
   const city = "college station";
   const apiKey = process.env.VITE_REACT_APP_WEATHER_KEY;
-  const APIUrl = "https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}";
+  const APIUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
   try {
     const weather = await axios.get(APIUrl);
     res.json(weather);
